2cebbf5421b4590deace5076fd90a0b4c3d78562
Ailen <mariaailenjuarez@gmail.com>
2025-09-12 11:10:32 -0300

    Tiempo

--- src/main/java/entity/Player.java
+++ src/main/java/entity/Player.java
@@ -9,18 +9,21 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public class Player extends Entity {
+
     gamePanel gp;
     KeyHandler keyH;
-
 
     public final int screenX;
     public final int screenY;
 
+    // Contadores de objetos
+    public int gpsCount = 0;
+    public int panDeAjoCount = 0;
+    public int valePorComidaCount = 0;
 
     public Player(gamePanel gp, KeyHandler keyH) {
         this.gp = gp;
         this.keyH = keyH;
-
 
         screenX = gp.screenWidth / 2 - (gp.tileSize / 2);
         screenY = gp.screenHeight / 2 - (gp.tileSize / 2);
@@ -28,33 +31,25 @@ public class Player extends Entity {
         solidArea = new Rectangle();
         solidArea.x = 8;
         solidArea.y = 16;
-
-        solidAreaDefaultX = solidArea.x;
-        solidAreaDefaultY = solidArea.y;
-
         solidArea.width = 32;
         solidArea.height = 32;
 
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
 
         setDefaultValues();
         getPlayersImage();
     }
 
     public void setDefaultValues() {
-
-
         worldX = gp.tileSize * 23;
         worldY = gp.tileSize * 21;
         speed = 4;
         direction = "Abajo";
-
     }
 
-    //Metodo para usar las imagenes
     public void getPlayersImage() {
         try {
-
-
             up1 = ImageIO.read(getClass().getResourceAsStream("/player/arriba.png"));
             up2 = ImageIO.read(getClass().getResourceAsStream("/player/arriba2.png"));
             down1 = ImageIO.read(getClass().getResourceAsStream("/player/pixil-frame-0 (1).png"));
@@ -63,115 +58,87 @@ public class Player extends Entity {
             left2 = ImageIO.read(getClass().getResourceAsStream("/player/izquierda2.png"));
             right1 = ImageIO.read(getClass().getResourceAsStream("/player/derecha.png"));
             right2 = ImageIO.read(getClass().getResourceAsStream("/player/derecha2.png"));
-
-
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    //Metodo para mandar los controles del jugador
     public void update() {
-        if (keyH.upPressed == true ||
-                keyH.downPressed == true ||
-                keyH.leftPressed == true ||
-                keyH.rightPressed == true) {
-            if (keyH.upPressed == true) {
-                direction = "Arriba";
+        if (keyH.upPressed || keyH.downPressed || keyH.leftPressed || keyH.rightPressed) {
+            if (keyH.upPressed) direction = "Arriba";
+            else if (keyH.downPressed) direction = "Abajo";
+            else if (keyH.leftPressed) direction = "Izquierda";
+            else if (keyH.rightPressed) direction = "Derecha";
 
-            } else if (keyH.downPressed == true) {
-                direction = "Abajo";
-            } else if (keyH.leftPressed == true) {
-                direction = "Izquierda";
-            } else if (keyH.rightPressed == true) {
-                direction = "Derecha";
-            }
-//check tile collision
             collisionOn = false;
             gp.cChecker.checkTile(this);
 
-
-            // check colision de objeto
             int objIndex = gp.cChecker.checkObject(this, true);
             pickUpObject(objIndex);
 
-// si la collision false, el jugador se puede mover
-            if (collisionOn == false) {
+            if (!collisionOn) {
                 switch (direction) {
-                    case "Arriba":
-                        worldY -= speed;
-                        break;
-                    case "Abajo":
-                        worldY += speed;
-                        break;
-                    case "Izquierda":
-                        worldX -= speed;
-                        break;
-                    case "Derecha":
-                        worldX += speed;
-                        break;
+                    case "Arriba" -> worldY -= speed;
+                    case "Abajo" -> worldY += speed;
+                    case "Izquierda" -> worldX -= speed;
+                    case "Derecha" -> worldX += speed;
                 }
-
             }
-
 
             spriteCounter++;
             if (spriteCounter > 10) {
-                if (spriteNum == 1) {
-                    spriteNum = 2;
-                } else if (spriteNum == 2) {
-                    spriteNum = 1;
-                }
+                spriteNum = (spriteNum == 1) ? 2 : 1;
                 spriteCounter = 0;
             }
         }
     }
 
-    public void  pickUpObject( int i){
-        if(i != 999){
-            gp.obj[i] = null;
+    // Método para recoger objetos, insensible a mayúsculas y espacios
+    public void pickUpObject(int i) {
+        if (i != 999 && gp.obj[i] != null) {
+
+            String objectName = gp.obj[i].name.toLowerCase().trim();
+            System.out.println("Objeto detectado: " + objectName);
+
+            if (objectName.contains("gps")) {
+                gpsCount++;
+                gp.obj[i] = null;
+                gp.playSE(1);
+                gp.ui.showMessage("Tienes el GPS de la nave!!");
+                System.out.println("GPS: " + gpsCount);
+
+            } else if (objectName.contains("pan de ajo")) {
+                panDeAjoCount++;
+                gp.obj[i] = null;
+                gp.playSE(1);
+                gp.ui.showMessage("Tienes el pan de ajo!!");
+                System.out.println("Pan de Ajo: " + panDeAjoCount);
+
+            } else if (objectName.contains("vale por comida")) {
+                valePorComidaCount++;
+                gp.obj[i] = null;
+                gp.playSE(1);
+                speed += 1;
+                gp.ui.showMessage("Tienes el vale por comida!!");
+                System.out.println("Vale por comida: " + valePorComidaCount);
+
+            } else if (objectName.contains("chest")) {
+                gp.ui.gameFinished = true;
+                gp.stopMusic();
+                gp.playSE(3);
+            }
         }
     }
 
     public void draw(Graphics2D g2) {
-
         BufferedImage image = null;
         switch (direction) {
-            case "Arriba":
-                if (spriteNum == 1) {
-                    image = up1;
-                }
-                if (spriteNum == 2) {
-                    image = up2;
-                }
-                break;
-            case "Abajo":
-                if (spriteNum == 1) {
-                    image = down1;
-                }
-                if (spriteNum == 2) {
-                    image = down2;
-                }
-                break;
-            case "Izquierda":
-                if (spriteNum == 1) {
-                    image = left1;
-                }
-                if (spriteNum == 2) {
-                    image = left2;
-                }
-                break;
-            case "Derecha":
-                if (spriteNum == 1) {
-                    image = right1;
-                }
-                if (spriteNum == 2) {
-                    image = right2;
-                }
-                break;
+            case "Arriba" -> image = (spriteNum == 1) ? up1 : up2;
+            case "Abajo" -> image = (spriteNum == 1) ? down1 : down2;
+            case "Izquierda" -> image = (spriteNum == 1) ? left1 : left2;
+            case "Derecha" -> image = (spriteNum == 1) ? right1 : right2;
         }
-
         g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
-
     }
 }
+
---
+++ src/main/java/main/UI.java
@@ -0,0 +1,100 @@
+package main;
+
+import object.OBJ_gpsNave;
+import object.OBJ_panDeAjo;
+import object.OBJ_valePorComida;
+
+import java.awt.*;
+import java.text.DecimalFormat;
+
+public class UI {
+
+    gamePanel gp;
+    Font arial_40, arial_80B;
+
+    // Mensajes
+    public boolean messageOn = false;
+    public String message = "";
+    int messageCounter = 0;
+
+    // Estado del juego
+    public boolean gameFinished = false;
+
+    // Tiempo de juego
+    double playTime;
+    DecimalFormat dFormat = new DecimalFormat("#0.00");
+
+    public UI(gamePanel gp) {
+        this.gp = gp;
+
+        arial_40 = new Font("Arial", Font.PLAIN, 20);
+        arial_80B = new Font("Arial", Font.BOLD, 40); // fuente más grande para victoria
+    }
+
+    // Mostrar mensaje temporal
+    public void showMessage(String text) {
+        message = text;
+        messageOn = true;
+    }
+
+    public void draw(Graphics2D g2) {
+
+        // -------------------
+        // HUD siempre visible
+        // -------------------
+        g2.setFont(arial_40);
+        g2.setColor(Color.white);
+
+        // Mostrar cantidad de llaves / objetos
+        g2.drawString("GPS: " + gp.player.gpsCount, 25, 50);
+        g2.drawString("Pan de Ajo: " + gp.player.panDeAjoCount, 25, 70);
+        g2.drawString("Vale por comida: " + gp.player.valePorComidaCount, 25, 90);
+
+        // Mostrar tiempo de juego
+        playTime += 1.0 / 60;
+        g2.drawString("Time: " + dFormat.format(playTime), gp.tileSize * 11, 65);
+
+        // -------------------
+        // Mensaje temporal
+        // -------------------
+        if (messageOn) {
+            g2.setFont(g2.getFont().deriveFont(30F));
+            g2.setColor(Color.YELLOW);
+            g2.drawString(message, gp.tileSize / 2, gp.tileSize * 5);
+
+            messageCounter++;
+            if (messageCounter > 120) { // 2 segundos aprox
+                messageCounter = 0;
+                messageOn = false;
+            }
+        }
+
+        // -------------------
+        // Mensaje de victoria
+        // -------------------
+        if (gameFinished) {
+            g2.setFont(arial_80B);
+            g2.setColor(Color.YELLOW);
+
+            String text = "Felicidades, has ganado!!";
+            int textLength = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+            int x = gp.screenWidth / 2 - textLength / 2;
+            int y = gp.screenHeight / 2;
+            g2.drawString(text, x, y);
+
+            text = "Has encontrado a Peivon!!";
+            textLength = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+            x = gp.screenWidth / 2 - textLength / 2;
+            y = gp.screenHeight / 2 - (gp.tileSize * 3);
+            g2.drawString(text, x, y);
+
+            text = "Your Time is: " + dFormat.format(playTime);
+            textLength = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+            x = gp.screenWidth / 2 - textLength / 2;
+            y = gp.screenHeight / 2 - (gp.tileSize * 4);
+            g2.drawString(text, x, y);
+
+            // NO detener el gameThread automáticamente
+        }
+    }
+}
